92f018e4af0fcfce1daa336defe09ad60f8c5c47
John Gallaugher <john.gallaugher@bc.edu>
2019-06-10 16:23:10 +0100

    FounderButtons from Day 1 of #BCSwiftDublin 2019

--- MetricHelp/ViewController.swift
+++ MetricHelp/ViewController.swift
@@ -9,12 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var distanceField: UITextField!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var useMetricSwitch: UISwitch!
+    @IBOutlet weak var resultsLabel: UILabel!
+    let milesToKm = 1.60934
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func toggleMetricSwitch(_ sender: UISwitch) {
+        if useMetricSwitch.isOn {
+            distanceLabel.text = "miles"
+        } else {
+            distanceLabel.text = "kilometers"
+        }
+        resultsLabel.text = ""
+    }
+    
+    @IBAction func convertButtonPressed(_ sender: UIButton) {
+        if let distanceRun = Double(distanceField.text!) {
+            if useMetricSwitch.isOn {
+                let convertedRun = distanceRun * milesToKm
+                resultsLabel.text = "You ran \(distanceRun) miles. That's \(convertedRun) kilometers!"
+            } else {
+                let convertedRun = distanceRun / milesToKm
+                resultsLabel.text = "You ran \(distanceRun) kilometers. That's \(convertedRun) miles!"
+            }
+        } else {
+            // I couldn't convert to a Double. I got a nil
+            resultsLabel.text = "Please enter a valid number without any letters, spaces, or symbols."
+        }
+        
+        
+        
+        
+        
+        
+        
+        //        guard let distanceRun = Double(distanceField.text!) else  {
+        //            resultsLabel.text = "Please enter a valid number without any letters, spaces, or symbols."
+        //            return
+        //        }
+        //        if useMetricSwitch.isOn {
+        //            let convertedRun = distanceRun * milesToKm
+        //            resultsLabel.text = "You ran \(distanceRun) miles. That's \(convertedRun) kilometers!"
+        //        } else {
+        //            let convertedRun = distanceRun / milesToKm
+        //            resultsLabel.text = "You ran \(distanceRun) kilometers. That's \(convertedRun) miles!"
+        //        }
+    }
 }
 
